779cc1a5dad14f3681d7c3b2ddbe71d5da0eebd0
Alisonhardison <appletini19@gmail.com>
2019-11-18 15:28:22 +0000

    initial commit

---
+++ recursive_fib.rb
@@ -0,0 +1,29 @@
+def recursive_fib (n)
+  return n if n <= 1
+  recursive_fib(n-1) + recursive_fib(n-2)
+end
+
+#puts recursive_fib (10)
+
+def iterative_fib(n)
+  previous = 0
+  current = 1
+  n.times do
+    temp = current
+    current = current + previous
+    previous = temp
+  end
+  return previous
+end
+
+#puts iterative_fib (10)
+
+
+require 'benchmark'
+num = 10
+Benchmark.bm do |x|
+  x.report("recursive_fib") { recursive_fib(num) }
+  x.report("iterative_fib")  { iterative_fib(num)  }
+end
+
+
