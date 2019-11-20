9a9f76757166a2527cb1c5b49d3ef2944a11e53c
Alisonhardison <appletini19@gmail.com>
2019-11-20 14:40:27 +0000

    updated benchmark

--- recursive_fib.rb
+++ recursive_fib.rb
@@ -3,24 +3,24 @@ def recursive_fib (n)
   recursive_fib(n-1) + recursive_fib(n-2)
 end
 
-#puts recursive_fib (10)
+puts recursive_fib (10)
 
 def iterative_fib(n)
   previous = 0
   current = 1
   n.times do
-    temp = current
+    num = current
     current = current + previous
-    previous = temp
+    previous = num
   end
   return previous
 end
 
-#puts iterative_fib (10)
+puts iterative_fib (10)
 
 
 require 'benchmark'
-num = 10
+num = 35
 Benchmark.bm do |x|
   x.report("recursive_fib") { recursive_fib(num) }
   x.report("iterative_fib")  { iterative_fib(num)  }
